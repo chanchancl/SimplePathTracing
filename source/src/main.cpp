c55a117a377902c4cb5df114784f652204f121a0
Ccl <ccl.love.miku@gmail.com>
2025-08-20 17:22:01 +0800

    2. threadpool and film

--- source/src/main.cpp
+++ source/src/main.cpp
@@ -1,11 +1,31 @@
-#include "glm/fwd.hpp"
+
+
 #include <glm/glm.hpp>
 #include <iostream>
 
+#include "camera.hpp"
+#include "film.hpp"
+#include "glm/geometric.hpp"
+#include "sphere.hpp"
+#include "threadpool.hpp"
+
 using namespace std;
 
+class SimpleTask : public Task {
+public:
+  virtual void run() override { cout << "Hello World!" << endl; }
+};
+
 int main() {
-  glm::vec3 test;
-  cout << "Hello world" << endl;
+  ThreadPool pool{};
+  Film film(800, 450);
+
+  pool.parallelFor(
+      200, 100, [&](auto x, auto y) { film.setPixel(x, y, {0.6, 0.7, 0.2}); });
+
+  pool.wait();
+
+  film.save("test.ppm");
+
   return 0;
 }
